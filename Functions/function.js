4f440a071724d7c994aa5bc8704c6502e7844c47
Nick <nyfrase3@live.com>
2023-07-03 16:47:45 -0400

    moved the rest of the functions lost from earlier merge

--- Functions/function.js
+++ Functions/function.js
@@ -1,6 +1,14 @@
 const ROWSPERPAGE = 500; // A global constant that stores the amount of documents to serve
 const INITIALSORTFIELD = 'name'; //String to hold the sort criteria for initial queries
  
+// Function call to import all data from the database
+async function allData(coll) {
+
+
+  const data = await coll.find().sort({[INITIALSORTFIELD]: 1, _id: 1}).limit(ROWSPERPAGE).toArray(); 
+  return data;
+}
+
  async function getByGrowthRate(coll, low=false) {
 let sortInt = -1;
   if (low) {
@@ -156,6 +164,101 @@ async function topTen(gender, coll) {
   return data;
 }
 
+async function getByRank(name, rank, coll) {
+
+  const findQuery = {name, 'rank': {$lte: rank}};
+  const options = { 
+    sort: {'rank': 1},
+  };
+
+  const data = await coll.find(findQuery, options).toArray();
+
+  console.log(data);
+  return data;
+
+}
+
+
+async function getSingleName( name, coll ) {
+  const findQuery = {name}
+  // console.log(findQuery)
+  const data = await coll.find(findQuery).sort({"year": 1}).toArray();
+ // console.log(data);
+  return data;
+}
+
+
+// Aggregates all names within a certain year, regardless of race
+async function allDataJoined(name, coll) {
+
+  const pipeline = [
+  {
+     '$match': {
+       'name': {
+         '$regex': new RegExp(`^${name}`, 'i')
+       }
+     }
+   },  
+  {
+   '$bucket': {
+       'groupBy': '$year',
+       'boundaries': [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+       'default': 2019,
+       'output': {
+          "Total" : { '$count': {} },
+          "TotalNumber": { '$sum': '$number'},
+          "Name": { '$push': '$name'},
+          "Rank": {'$avg': {'$sum': '$rank'} }
+
+         }      
+     }      
+  } 
+]
+ const data = await coll.aggregate(pipeline).toArray();
+
+ console.log(data);
+}
+
+async function allDataNum(field, page, order, gender, coll) {
+  let data;
+  const skip = +page * ROWSPERPAGE;
+  let sortInt;
+  let sortQuery = {};
+  let findQuery = {};
+  if (field == "name") { //Here name is the only string type 
+  
+    if (order == 'desc') {
+        sortInt = 1;
+    } else {
+      sortInt = -1;
+      }
+    } else { // Here field is of type int 
+      if (order == 'desc') {
+      sortInt = -1;
+    } else {
+      sortInt = 1;
+      }
+    }
+
+      sortQuery[field] = sortInt;
+      
+  
+      if (field == 'rank') { // if we are sorting based on rank sort it secondly by number in the opposite order so if Rank 1 is first the highest number of all ranks is listed first
+        sortQuery['number'] = -sortInt;
+      } 
+        sortQuery['_id'] = 1;
+      
+
+      if (gender == 'm'){
+        findQuery['gender'] = 'Male';
+      } else if (gender == 'f') {
+        findQuery['gender'] = 'Female';
+      }
+
+      data = await coll.find(findQuery).sort(sortQuery).limit(ROWSPERPAGE).skip(skip).toArray();
+      return data;
+}
+
 module.exports = {
-    getByGrowthRate, allData, allDataJoined, allDataNum, getSingleName, getByNames, getByRank, topTen
+    getByGrowthRate, allData, allDataNum, getSingleName, getByNames, getByRank, topTen
 }
--- app.js
+++ app.js
@@ -5,9 +5,12 @@
 
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { allData, getByGrowthRate, allDataNum, getByNames, getByRank, getSingleName, topTen } = require('./Functions/function.js');
 
-const { allData, getByGrowthRate, allDataJoined, allDataNum, getByNames, getByRank, getSingleName, topTen } = require('./Functions/function.js');
 const app = express();
+
+
 const uri = "mongodb+srv://Admin:nyjd-2023@nyjd.xa26v9a.mongodb.net/?retryWrites=true&w=majority";
 app.use(cors());
 
@@ -32,14 +35,14 @@ app.get('/', (req, res) => {
 
 app.get('/allData', async (req, res) =>  { 
 
-  const data = await allData(); 
+  const data = await allData(myColl); 
   res.json(data);
 });
 
 app.get('/allData/:field/:pageNumber/:order/:gender', async (req, res) =>  { 
   
   const { field, pageNumber, order, gender } = req.params;
-  const data = await allDataNum(field, pageNumber, order, gender); 
+  const data = await allDataNum(field, pageNumber, order, gender, myColl); 
   res.json(data);
  
 });
@@ -48,14 +51,14 @@ app.get('/names/:searchTerm', async (req, res) =>  {
   const { searchTerm } = req.params;
   const names = await getByNames(searchTerm, myColl); 
   // Needs a route
-  // await allDataJoined(searchTerm);
+  // await allDataJoined(searchTerm, mycoll);
   res.json(names);
  
 });
 
 app.get('/name/:name', async (req, res) =>  { 
   const { name } = req.params;
-  const names = await getSingleName(name); 
+  const names = await getSingleName(name, myColl); 
   // const ranks = await getByRank(name, 10);
   res.json(names);
  
@@ -64,7 +67,7 @@ app.get('/name/:name', async (req, res) =>  {
 // Gets names for a certain rank and above
 app.get('/name/:name/:rank', async (req, res) => {
   const { name, rank } = req.params;
-  const names = await getByRank(name, rank);
+  const names = await getByRank(name, rank, myColl);
 
   res.json(names);
 });
@@ -90,8 +93,6 @@ app.get('/botTenGrowthRate', async (req, res) => {
 });
 
 
-
-
 // connect to the database
 async function run() {
     try {
@@ -107,105 +108,3 @@ async function run() {
     }
 }
   run().catch(console.dir);
-
-// Function call to import all data from the database
-async function allData() {
-
-
-  const data = await myColl.find().sort({[INITIALSORTFIELD]: 1, _id: 1}).limit(ROWSPERPAGE).toArray(); 
-  return data;
-}
-
-// Aggregates all names within a certain year, regardless of race
-async function allDataJoined(name) {
-
-   const pipeline = [
-   {
-      '$match': {
-        'name': {
-          '$regex': new RegExp(`^${name}`, 'i')
-        }
-      }
-    },  
-   {
-    '$bucket': {
-        'groupBy': '$year',
-        'boundaries': [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
-        'default': 2019,
-        'output': {
-           "Total" : { '$count': {} },
-           "TotalNumber": { '$sum': '$number'},
-           "Name": { '$push': '$name'},
-           "Rank": {'$avg': {'$sum': '$rank'} }
-
-          }      
-      }      
-   } 
-]
-  const data = await myColl.aggregate(pipeline).toArray();
-
-  console.log(data);
-}
-
-async function allDataNum(field, page, order, gender) {
-  let data;
-  const skip = +page * ROWSPERPAGE;
-  let sortInt;
-  let sortQuery = {};
-  let findQuery = {};
-  if (field == "name") { //Here name is the only string type 
-  
-    if (order == 'desc') {
-        sortInt = 1;
-    } else {
-      sortInt = -1;
-      }
-    } else { // Here field is of type int 
-      if (order == 'desc') {
-      sortInt = -1;
-    } else {
-      sortInt = 1;
-      }
-    }
-
-      sortQuery[field] = sortInt;
-      
-  
-      if (field == 'rank') { // if we are sorting based on rank sort it secondly by number in the opposite order so if Rank 1 is first the highest number of all ranks is listed first
-        sortQuery['number'] = -sortInt;
-      } 
-        sortQuery['_id'] = 1;
-      
-
-      if (gender == 'm'){
-        findQuery['gender'] = 'Male';
-      } else if (gender == 'f') {
-        findQuery['gender'] = 'Female';
-      }
-
-      data = await myColl.find(findQuery).sort(sortQuery).limit(ROWSPERPAGE).skip(skip).toArray();
-      return data;
-}
-
-async function getByRank(name, rank) {
-
-  const findQuery = {name, 'rank': {$lte: rank}};
-  const options = { 
-    sort: {'rank': 1},
-  };
-
-  const data = await myColl.find(findQuery, options).toArray();
-
-  console.log(data);
-  return data;
-
-}
-
-
-async function getSingleName( name ) {
-  const findQuery = {name}
-  // console.log(findQuery)
-  const data = await myColl.find(findQuery).sort({"year": 1}).toArray();
- // console.log(data);
-  return data;
-}
